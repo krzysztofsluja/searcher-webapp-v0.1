e97880338ffa28c750942d63e0513f7cdd6b9ab4
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-06-13 00:26:43 +0200

    #86bz4h0bv - refactor - delete all unused classes

--- src/main/java/org/sluja/searcher/webapp/utils/website/WebsiteHandler.java
+++
@@ -1,8 +0,0 @@
-package org.sluja.searcher.webapp.utils.website;
-
-import org.sluja.searcher.webapp.exception.action.WebsiteUnsuccessfulActionException;
-
-public interface WebsiteHandler<T> {
-
-    void doAction(T element) throws WebsiteUnsuccessfulActionException;
-}
