438b6de273ec155adc355978063eadf018040f7f
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-06-22 09:54:08 +0200

    #86bz7y8n3 - add connector class and request class for dynamic websites

---
+++ src/main/java/org/sluja/searcher/webapp/service/connector/dynamic/DynamicWebsiteConnector.java
@@ -0,0 +1,25 @@
+package org.sluja.searcher.webapp.service.connector.dynamic;
+
+import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.sluja.searcher.webapp.dto.connect.DynamicWebsiteConnectRequest;
+import org.sluja.searcher.webapp.exception.connection.ConnectionTimeoutException;
+import org.sluja.searcher.webapp.utils.connector.IConnector;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class DynamicWebsiteConnector implements IConnector<WebDriver, DynamicWebsiteConnectRequest> {
+
+    public static DynamicWebsiteConnector INSTANCE = new DynamicWebsiteConnector();
+    @Override
+    public WebDriver connectAndGetPage(final DynamicWebsiteConnectRequest request) throws ConnectionTimeoutException, IOException {
+        if(StringUtils.isEmpty(request.getUrl())) {
+            throw new ConnectionTimeoutException();
+        }
+        final WebDriver driver = Objects.nonNull(request.getDriver()) ? request.getDriver() : new ChromeDriver();
+        driver.get(request.getUrl());
+        return driver;
+    }
+}
