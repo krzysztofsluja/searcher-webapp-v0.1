6cf4c53f1cabe11214c02fd811a6b44d7ff6b3db
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-06-22 19:47:26 +0200

    #86bz7y8n3 - refactor - change declaration of the getProperty method - it will consume request and property name

--- src/main/java/org/sluja/searcher/webapp/service/scraper/search/BaseSearchService.java
+++ src/main/java/org/sluja/searcher/webapp/service/scraper/search/BaseSearchService.java
@@ -1,12 +1,22 @@
 package org.sluja.searcher.webapp.service.scraper.search;
 
-import org.sluja.searcher.webapp.dto.scraper.ScrapRequest;
+import org.sluja.searcher.webapp.dto.scraper.search.SearchRequest;
+import org.sluja.searcher.webapp.enums.scraper.search.SearchProperty;
 import org.sluja.searcher.webapp.exception.enums.search.ValueForSearchPropertyException;
+import org.sluja.searcher.webapp.exception.product.general.ProductNotFoundException;
 import org.sluja.searcher.webapp.service.enums.search.SearchPropertyGetComponent;
+import java.util.List;
+import java.util.Objects;
 
-public abstract class BaseSearchService {
+public abstract class BaseSearchService<T extends SearchRequest> {
 
-    protected Object getProperty(final Object request) throws ValueForSearchPropertyException {
-        return SearchPropertyGetComponent.getProperty(request);
+    protected Object getProperty(final SearchRequest request, final SearchProperty property) throws ValueForSearchPropertyException {
+        final Object requestProperty = request.getProperties().get(property);
+        if(Objects.nonNull(requestProperty)) {
+           return SearchPropertyGetComponent.getProperty(requestProperty);
+        }
+        throw new ValueForSearchPropertyException();
     }
+
+    public abstract List<?> search(final T request) throws ProductNotFoundException;
 }
