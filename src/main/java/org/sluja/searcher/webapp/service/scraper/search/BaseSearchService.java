93a45edbcccc870a797262331a2a50fe321f15ce
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-06-23 16:46:25 +0200

    #86bz7y8n3 - add class for getting the property

--- src/main/java/org/sluja/searcher/webapp/service/scraper/search/BaseSearchService.java
+++ src/main/java/org/sluja/searcher/webapp/service/scraper/search/BaseSearchService.java
@@ -1,29 +1,25 @@
 package org.sluja.searcher.webapp.service.scraper.search;
 
+import org.sluja.searcher.webapp.dto.product.request.search.SearchServiceRequest;
 import org.sluja.searcher.webapp.dto.scraper.search.SearchRequest;
 import org.sluja.searcher.webapp.enums.scraper.search.SearchProperty;
 import org.sluja.searcher.webapp.exception.enums.search.ValueForSearchPropertyException;
 import org.sluja.searcher.webapp.exception.product.general.ProductNotFoundException;
-import org.sluja.searcher.webapp.service.enums.search.SearchPropertyGetComponent;
+import org.sluja.searcher.webapp.utils.property.GetSearchPropertyValueUtils;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
-public abstract class BaseSearchService<T extends SearchRequest> {
+public abstract class BaseSearchService<T extends SearchServiceRequest> {
 
     protected Object getProperty(final SearchRequest request, final SearchProperty property) throws ValueForSearchPropertyException {
-        final Object requestProperty = request.getProperties().get(property);
-        if(Objects.nonNull(requestProperty)) {
-           return SearchPropertyGetComponent.getProperty(requestProperty);
-        }
-        throw new ValueForSearchPropertyException();
+        return GetSearchPropertyValueUtils.getProperty(request, property);
     }
 
     public List<?> searchList(final T request) throws ProductNotFoundException, UnsupportedOperationException {
         throw new UnsupportedOperationException("The search method returning List is not supported in that class!");
     }
-    public Map<?,?> searchMap(final T request) throws UnsupportedOperationException{
+    public Map<?,?> searchMap(final T request) throws UnsupportedOperationException, ProductNotFoundException {
         throw new UnsupportedOperationException("The search method returning Map is not supported in that class!");
     }
 }
