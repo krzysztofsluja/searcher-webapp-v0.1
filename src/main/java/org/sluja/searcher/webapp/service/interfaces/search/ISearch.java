48f61c607952064b559d1bf0c2c477b8b44b2df2
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-06-25 23:25:42 +0200

    #86bz7y8n3 - add classes for all categories searching

--- src/main/java/org/sluja/searcher/webapp/service/interfaces/search/ISearch.java
+++ src/main/java/org/sluja/searcher/webapp/service/interfaces/search/ISearch.java
@@ -1,7 +1,7 @@
 package org.sluja.searcher.webapp.service.interfaces.search;
 
+import org.sluja.searcher.webapp.dto.product.request.search.SearchServiceRequest;
 import org.sluja.searcher.webapp.dto.scraper.ScrapRequest;
-import org.sluja.searcher.webapp.dto.scraper.search.SearchRequest;
 import org.sluja.searcher.webapp.exception.enums.search.ValueForSearchPropertyException;
 import org.sluja.searcher.webapp.exception.product.general.ProductNotFoundException;
 import org.sluja.searcher.webapp.exception.scraper.ScraperIncorrectFieldException;
@@ -10,7 +10,7 @@ import org.sluja.searcher.webapp.service.scraper.interfaces.WebsiteScraper;
 
 import java.util.List;
 
-public interface ISearch<S extends SearchRequest,T extends ScrapRequest> {
+public interface ISearch<S extends SearchServiceRequest,T extends ScrapRequest> {
 
     default List<?> search(S request, T scrapRequest) throws ValueForSearchPropertyException, ProductNotFoundException, ScraperIncorrectFieldException {
         final WebsiteScraper scraperService = WebsiteScraperFactory.getScraper(request.isDynamicWebsite());
