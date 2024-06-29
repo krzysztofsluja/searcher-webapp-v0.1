43727db127fd6a8a55d811c41c06e795bcbede1c
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-06-30 00:31:33 +0200

    #86bzc55b5_1 - implement extractor for product addresses, name and price

--- src/main/java/org/sluja/searcher/webapp/utils/extractor/Extractor.java
+++ src/main/java/org/sluja/searcher/webapp/utils/extractor/Extractor.java
@@ -1,9 +1,10 @@
 package org.sluja.searcher.webapp.utils.extractor;
 
 import org.sluja.searcher.webapp.exception.format.UnsuccessfulFormatException;
-import org.sluja.searcher.webapp.exception.product.ProductNotFoundException;
+import org.sluja.searcher.webapp.exception.product.general.ProductNotFoundException;
+import org.sluja.searcher.webapp.exception.scraper.ScraperIncorrectFieldException;
 
 @FunctionalInterface
 public interface Extractor<E,V,T> {
-    E extract(final V object, final T objectTwo) throws UnsuccessfulFormatException, ProductNotFoundException;
+    E extract(final V object, final T objectTwo) throws UnsuccessfulFormatException, ProductNotFoundException, ScraperIncorrectFieldException;
 }
--- src/main/java/org/sluja/searcher/webapp/utils/extractor/implementation/ProductImageAddressExtractor.java
+++ src/main/java/org/sluja/searcher/webapp/utils/extractor/implementation/ProductImageAddressExtractor.java
@@ -2,39 +2,52 @@ package org.sluja.searcher.webapp.utils.extractor.implementation;
 
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jboss.logging.Logger;
 import org.jsoup.nodes.Element;
-
-import org.sluja.searcher.webapp.dto.scraper.ProductScrapWithDefinedAttributes;
+import org.sluja.searcher.webapp.dto.product.ProductDTO;
+import org.sluja.searcher.webapp.dto.product.request.search.object.BuildProductObjectRequest;
+import org.sluja.searcher.webapp.dto.scraper.stat.StaticWebsiteElementScrapRequest;
 import org.sluja.searcher.webapp.exception.format.UnsuccessfulFormatException;
-import org.sluja.searcher.webapp.exception.product.ProductNotFoundException;
+import org.sluja.searcher.webapp.exception.product.general.ProductNotFoundException;
+import org.sluja.searcher.webapp.exception.scraper.ScraperIncorrectFieldException;
+import org.sluja.searcher.webapp.service.scraper.interfaces.WebsiteScraper;
 import org.sluja.searcher.webapp.utils.extractor.Extractor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-
 import java.util.List;
+import java.util.Objects;
 
 @Component
 @Qualifier("productImageAddressExtractor")
 @RequiredArgsConstructor
-public class ProductImageAddressExtractor implements Extractor<List<String>, Element, ProductScrapWithDefinedAttributes> {
+public class ProductImageAddressExtractor implements Extractor<List<String>, Element, BuildProductObjectRequest> {
 
     private static final Logger LOGGER = Logger.getLogger(ProductImageAddressExtractor.class);
-    //private final ProductScraper productScraperService;
+    private final WebsiteScraper<Element, StaticWebsiteElementScrapRequest> staticWebsiteElementScraper;
 
     @Override
-    public List<String> extract(final Element element, final ProductScrapWithDefinedAttributes request) throws ProductNotFoundException, UnsuccessfulFormatException {
-      /*  List<Element> elements = productScraperService.scrapElementsByAttributes(element, request.productImageAddresses());
-        List<String> imageAddresses = elements.stream()
-                .map(e -> e.attr(request.productImageExtractAttribute()))
+    public List<String> extract(final Element element, final BuildProductObjectRequest request) throws ProductNotFoundException, UnsuccessfulFormatException {
+
+        final List<Element> elements = request.getProductImageAddresses()
+                .stream()
+                .filter(StringUtils::isNotEmpty)
+                .map(address -> {
+                    final StaticWebsiteElementScrapRequest scrapRequest = new StaticWebsiteElementScrapRequest(address, element);
+                    try {
+                        return staticWebsiteElementScraper.scrap(scrapRequest);
+                    } catch (ScraperIncorrectFieldException e) {
+                        //TODO logging
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
                 .toList();
-        if (imageAddresses.isEmpty()) {
-            LOGGER.info("Product doesn't have any image! Default image will be used");
-            //TODO add default image
-            //return ProductScrapWithDefinedAttributes.NO_IMAGE_ADDRESS();
-        }
-        return imageAddresses;*/
-        return null;
+        final List<String> imageAddresses = elements.stream()
+                .map(e -> e.attr(request.getProductImageExtractAttribute()))
+                .toList();
+        return CollectionUtils.isEmpty(imageAddresses) ? ProductDTO.NO_IMAGE_ADDRESS() : imageAddresses;
     }
 }
