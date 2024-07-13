fc68e337b4e98e7814482d9d49a5c8662c74da12
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-07-13 10:25:08 +0200

    #86bzdrh4e - implement response for shop with many categories

---
+++ src/main/java/org/sluja/searcher/webapp/dto/product/response/GetProductsForShopAndManyCategoriesResponse.java
@@ -0,0 +1,27 @@
+package org.sluja.searcher.webapp.dto.product.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
+import org.sluja.searcher.webapp.dto.product.ProductDTO;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Builder
+public class GetProductsForShopAndManyCategoriesResponse {
+
+    private String shopName;
+    private List<String> categories;
+    private Map<String, List<ProductDTO>> productsForCategory;
+
+    public static GetProductsForShopAndManyCategoriesResponse empty() {
+        return GetProductsForShopAndManyCategoriesResponse.builder()
+                .shopName(StringUtils.EMPTY)
+                .categories(Collections.emptyList())
+                .productsForCategory(Collections.emptyMap())
+                .build();
+    }
+}
