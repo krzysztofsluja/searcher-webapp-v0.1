79aa7edb8cc9834f9bce44cda9fe3ecaeed459c1
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-07-13 10:34:30 +0200

    #86bzdrh4e - refactor product price extractor - handling exception when scraper has found more than one price on product (most likely when there's discount price available)

--- src/main/java/org/sluja/searcher/webapp/utils/extractor/implementation/ProductPriceExtractor.java
+++ src/main/java/org/sluja/searcher/webapp/utils/extractor/implementation/ProductPriceExtractor.java
@@ -1,6 +1,7 @@
 package org.sluja.searcher.webapp.utils.extractor.implementation;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.nodes.Element;
 import org.sluja.searcher.webapp.dto.product.request.search.object.BuildProductObjectRequest;
 import org.sluja.searcher.webapp.dto.scraper.stat.StaticWebsiteElementScrapRequest;
@@ -12,6 +13,7 @@ import org.sluja.searcher.webapp.service.interfaces.scrap.IGetScraper;
 import org.sluja.searcher.webapp.service.scraper.interfaces.WebsiteScraper;
 import org.sluja.searcher.webapp.utils.extractor.Extractor;
 import org.sluja.searcher.webapp.utils.formatter.ProductFormatter;
+import org.sluja.searcher.webapp.utils.formatter.price.ProductPriceFormatter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
@@ -30,7 +32,18 @@ public class ProductPriceExtractor implements Extractor<BigDecimal, Element, Bui
     public BigDecimal extract(final Element element, final BuildProductObjectRequest request) throws UnsuccessfulFormatException, ScraperIncorrectFieldException {
         final StaticWebsiteElementScrapRequest scrapRequest = new StaticWebsiteElementScrapRequest(request.getProductPrice(), element);
         final String noFormattedPrice = formatPrice(request, getScraperService().scrap(scrapRequest));
-        return new BigDecimal(noFormattedPrice).setScale(RESOLUTION_PRICE, RoundingMode.HALF_UP);
+        try{
+            return new BigDecimal(noFormattedPrice).setScale(RESOLUTION_PRICE, RoundingMode.HALF_UP);
+        } catch (NumberFormatException ex) {
+            String price = element.select(request.getProductDiscountPrice())
+                    .text()
+                    .replaceAll(ProductPriceFormatter.REGEX, StringUtils.EMPTY);
+            if (StringUtils.isEmpty(price)) {
+                String content = request.getProductPrice();
+                price = element.attr(content);
+            }
+            return new BigDecimal(price).setScale(RESOLUTION_PRICE, RoundingMode.HALF_UP);
+        }
     }
 
     private String formatPrice(final BuildProductObjectRequest request, Element element) throws UnsuccessfulFormatException {
