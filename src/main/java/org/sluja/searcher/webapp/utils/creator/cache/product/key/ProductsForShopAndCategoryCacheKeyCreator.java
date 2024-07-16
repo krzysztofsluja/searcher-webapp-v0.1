56e495be024bf2d010673cb529361d22157ac02b
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-07-16 23:14:13 +0200

    #86bzk5n66 - add infrastructure for creating the cache key

---
+++ src/main/java/org/sluja/searcher/webapp/utils/creator/cache/product/key/ProductsForShopAndCategoryCacheKeyCreator.java
@@ -0,0 +1,34 @@
+package org.sluja.searcher.webapp.utils.creator.cache.product.key;
+
+import org.sluja.searcher.webapp.dto.creator.product.ProductsForShopAndCategoryKeyCreatorData;
+import org.sluja.searcher.webapp.exception.cache.CacheKeyCreationFailedException;
+import org.sluja.searcher.webapp.utils.creator.Creator;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+@Qualifier("productsForShopAndCategoryCacheKeyCreator")
+public class ProductsForShopAndCategoryCacheKeyCreator implements Creator<String, ProductsForShopAndCategoryKeyCreatorData> {
+
+    private static final String SEPARATOR = "-";
+
+    @Override
+    public String create(final ProductsForShopAndCategoryKeyCreatorData request) throws CacheKeyCreationFailedException {
+        if(Objects.nonNull(request)) {
+            return new StringBuilder()
+                    .append(request.shopName())
+                    .append(SEPARATOR)
+                    .append(request.category())
+                    .append(SEPARATOR)
+                    .append(request.additionDate().getYear())
+                    .append(SEPARATOR)
+                    .append(request.additionDate().getMonthValue())
+                    .append(SEPARATOR)
+                    .append(request.additionDate().getDayOfMonth())
+                    .toString();
+        }
+        throw new CacheKeyCreationFailedException();
+    }
+}
