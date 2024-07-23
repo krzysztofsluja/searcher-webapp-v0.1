a583b35ff6362fcf7674d370e0c992886bcebcd8
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-07-23 23:50:32 +0200

    #86bznb5h0 - add dtos and mappers for category and category properties

---
+++ src/main/java/org/sluja/searcher/webapp/mapper/category/CategoryPropertyMapper.java
@@ -0,0 +1,17 @@
+package org.sluja.searcher.webapp.mapper.category;
+
+import org.mapstruct.*;
+import org.sluja.searcher.webapp.dto.presentation.category.property.CategoryPropertyDto;
+import org.sluja.searcher.webapp.model.category.property.CategoryProperty;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
+public interface CategoryPropertyMapper {
+
+    @Mapping(source = "categoryProperty.category.name", target = "categoryName")
+    CategoryPropertyDto toDto(final CategoryProperty categoryProperty, @org.mapstruct.Context final String context);
+
+    @AfterMapping
+    default void setContextNameToDto(@MappingTarget final CategoryPropertyDto categoryPropertyDto, @org.mapstruct.Context final String context) {
+        categoryPropertyDto.setContext(context);
+    }
+}
