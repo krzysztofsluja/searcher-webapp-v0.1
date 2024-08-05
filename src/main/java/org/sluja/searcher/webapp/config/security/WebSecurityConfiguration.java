a85b4a9a25afa345c42cf01051e5aef14491e3ed
slujakrzysztof <krzysiek.sluja@vp.pl>
2024-08-05 14:52:47 +0200

    #86bzvr8t7 - add initial security configuration

---
+++ src/main/java/org/sluja/searcher/webapp/config/security/WebSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package org.sluja.searcher.webapp.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/test").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+
+}
