c00acf9de0f8a7eb62529f46d90578e555124b9e
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-09-04 09:36:59 +0200

    #86c05u9x6 - fix configuration - add ssl

--- src/main/java/org/sluja/searcher/webapp/config/email/EmailConfiguration.java
+++ src/main/java/org/sluja/searcher/webapp/config/email/EmailConfiguration.java
@@ -23,6 +23,7 @@ public class EmailConfiguration {
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
